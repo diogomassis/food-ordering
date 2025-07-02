051e74aadabc035c9e8d16693b00fd1869066962
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-02 19:37:29 -0300

    feat: Add constructor documentation for OrderItemId value object

--- food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/valueobject/OrderItemId.java
+++ food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/valueobject/OrderItemId.java
@@ -2,7 +2,17 @@ package com.food.ordering.system.order.service.domain.valueobject;
 
 import com.food.ordering.system.domain.valueobject.BaseId;
 
+/**
+ * Value object representing the unique identifier for an Order Item.
+ * Extends {@link BaseId} with a {@link Long} value type.
+ * Used to uniquely identify items within an order in the food ordering system.
+ */
 public class OrderItemId extends BaseId<Long> {
+    /**
+     * Constructs a new {@code OrderItemId} with the specified value.
+     *
+     * @param value the unique identifier for the order item
+     */
     protected OrderItemId(Long value) {
         super(value);
     }
