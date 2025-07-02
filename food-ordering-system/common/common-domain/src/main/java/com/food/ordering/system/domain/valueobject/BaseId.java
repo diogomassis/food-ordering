dd1f6de805fc8a1164c3d6338f39a37add2f60e1
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-01 23:50:03 -0300

    feat: Add BaseId abstract class for value object representation

---
+++ food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/BaseId.java
@@ -0,0 +1,30 @@
+package com.food.ordering.system.domain.valueobject;
+
+import java.util.Objects;
+
+public abstract class BaseId<T> {
+    private final T value;
+
+    protected BaseId(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        BaseId<?> baseId = (BaseId<?>) obj;
+        return value.equals(baseId.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+}
