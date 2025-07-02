e6584200deeadc4291e4bf4d9749c3d7376543c5
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-02 00:32:40 -0300

    feat: Enhance IDomainEvent interface documentation for clarity and understanding

--- food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/event/IDomainEvent.java
+++ food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/event/IDomainEvent.java
@@ -1,5 +1,15 @@
 package com.food.ordering.system.domain.event;
 
+/**
+ * Represents a domain event in the system.
+ * <p>
+ * Domain events are used to capture and communicate significant occurrences
+ * within the domain.
+ * Implementations of this interface should define the specific event details
+ * and payload.
+ *
+ * @param <T> the type of the entity or aggregate root associated with the event
+ */
 public interface IDomainEvent<T> {
 
 }
