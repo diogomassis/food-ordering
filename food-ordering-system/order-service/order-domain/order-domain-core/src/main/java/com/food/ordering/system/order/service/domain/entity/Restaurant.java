60ea02c7bbbc3b106ad2ef62280dcb29345c85f3
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-03 20:49:47 -0300

    feat: Enhance Restaurant entity with builder pattern

--- food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Restaurant.java
+++ food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Restaurant.java
@@ -1,8 +1,57 @@
 package com.food.ordering.system.order.service.domain.entity;
 
+import java.util.List;
+
 import com.food.ordering.system.domain.entity.AggregateRoot;
 import com.food.ordering.system.domain.valueobject.RestaurantId;
 
 public class Restaurant extends AggregateRoot<RestaurantId> {
+    private final List<Product> products;
+    private boolean active;
 
+    private Restaurant(Builder builder) {
+        setId(builder.restaurantId);
+        products = builder.products;
+        active = builder.active;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static final class Builder {
+        private RestaurantId restaurantId;
+        private List<Product> products;
+        private boolean active;
+
+        private Builder() {
+        }
+
+        public Builder restaurantId(RestaurantId restaurantId) {
+            this.restaurantId = restaurantId;
+            return this;
+        }
+
+        public Builder products(List<Product> products) {
+            this.products = products;
+            return this;
+        }
+
+        public Builder active(boolean active) {
+            this.active = active;
+            return this;
+        }
+
+        public Restaurant build() {
+            return new Restaurant(this);
+        }
+    }
 }
