6b193b25b84fa42313b04b6add34f9e0df64b34e
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-05 14:50:13 -0300

    docs: Update documentation for OrderPaidRestaurantRequestMessagePublisher interface

--- food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/restaurantapproval/OrderPaidRestaurantRequestMessagePublisher.java
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/restaurantapproval/OrderPaidRestaurantRequestMessagePublisher.java
@@ -3,6 +3,11 @@ package com.food.ordering.system.order.service.domain.ports.output.message.publi
 import com.food.ordering.system.domain.event.publisher.IDomainEventPublisher;
 import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
 
+/**
+ * Publisher interface for sending restaurant approval request messages when an
+ * order is paid.
+ * Extends {@link IDomainEventPublisher} for {@link OrderPaidEvent}.
+ */
 public interface OrderPaidRestaurantRequestMessagePublisher extends IDomainEventPublisher<OrderPaidEvent> {
 
 }
