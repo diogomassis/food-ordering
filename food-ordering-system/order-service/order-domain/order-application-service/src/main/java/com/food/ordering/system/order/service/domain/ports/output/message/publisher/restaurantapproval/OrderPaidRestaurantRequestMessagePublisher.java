d275c09107b65d916355bb1766a4c5a0a2bab0e2
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-05 14:50:06 -0300

    feat: Add OrderPaidRestaurantRequestMessagePublisher interface for handling restaurant approval requests upon order payment

---
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/restaurantapproval/OrderPaidRestaurantRequestMessagePublisher.java
@@ -0,0 +1,8 @@
+package com.food.ordering.system.order.service.domain.ports.output.message.publisher.restaurantapproval;
+
+import com.food.ordering.system.domain.event.publisher.IDomainEventPublisher;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
+
+public interface OrderPaidRestaurantRequestMessagePublisher extends IDomainEventPublisher<OrderPaidEvent> {
+
+}
