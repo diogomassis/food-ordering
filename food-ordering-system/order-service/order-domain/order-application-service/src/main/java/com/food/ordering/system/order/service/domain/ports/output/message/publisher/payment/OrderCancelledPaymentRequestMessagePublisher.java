3418b0182608d9f047eb8e599022cd483b289ddb
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-05 14:49:44 -0300

    docs: Add documentation for OrderCancelledPaymentRequestMessagePublisher and OrderCreatedPaymentRequestMessagePublisher interfaces

--- food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/payment/OrderCancelledPaymentRequestMessagePublisher.java
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/payment/OrderCancelledPaymentRequestMessagePublisher.java
@@ -3,6 +3,11 @@ package com.food.ordering.system.order.service.domain.ports.output.message.publi
 import com.food.ordering.system.domain.event.publisher.IDomainEventPublisher;
 import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
 
+/**
+ * Publisher interface for sending payment request messages when an order is
+ * cancelled.
+ * Extends {@link IDomainEventPublisher} for {@link OrderCancelledEvent}.
+ */
 public interface OrderCancelledPaymentRequestMessagePublisher extends IDomainEventPublisher<OrderCancelledEvent> {
 
 }
--- food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/payment/OrderCreatedPaymentRequestMessagePublisher.java
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/payment/OrderCreatedPaymentRequestMessagePublisher.java
@@ -3,6 +3,11 @@ package com.food.ordering.system.order.service.domain.ports.output.message.publi
 import com.food.ordering.system.domain.event.publisher.IDomainEventPublisher;
 import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
 
+/**
+ * Publisher interface for sending payment request messages when an order is
+ * created.
+ * Extends {@link IDomainEventPublisher} for {@link OrderCreatedEvent}.
+ */
 public interface OrderCreatedPaymentRequestMessagePublisher extends IDomainEventPublisher<OrderCreatedEvent> {
 
 }
