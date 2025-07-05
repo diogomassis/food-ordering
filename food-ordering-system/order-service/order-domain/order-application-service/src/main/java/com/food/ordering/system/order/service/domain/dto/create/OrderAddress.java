1902ba5181262297e2c6c3e1b44f4e3adbd807e6
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-05 13:00:30 -0300

    feat: Add documentation comments in OrderAddress class

--- food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/OrderAddress.java
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/OrderAddress.java
@@ -6,18 +6,32 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+/**
+ * Value object representing the delivery address for an order.
+ * <p>
+ * Contains street, postal code, and city information.
+ */
 @Getter
 @Builder
 @AllArgsConstructor
 public class OrderAddress {
+    /**
+     * Street name of the delivery address (max 50 characters).
+     */
     @NotNull
     @Max(value = 50)
     private final String street;
 
+    /**
+     * Postal code of the delivery address (max 10 characters).
+     */
     @NotNull
     @Max(value = 10)
     private final String postalCode;
 
+    /**
+     * City of the delivery address (max 50 characters).
+     */
     @NotNull
     @Max(value = 50)
     private final String city;
