21d875839879a7b059e5e7015c82c34ea4cb4636
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-05 12:59:05 -0300

    docs: Add documentation comments in CreateOrderCommand DTO

--- food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/CreateOrderCommand.java
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/dto/create/CreateOrderCommand.java
@@ -9,22 +9,44 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+/**
+ * Command object for creating a new order.
+ * <p>
+ * Contains all necessary information to create an order, including customer and
+ * restaurant IDs,
+ * total price, list of order items, and delivery address.
+ */
 @Getter
 @Builder
 @AllArgsConstructor
 public class CreateOrderCommand {
+    /**
+     * Unique identifier of the customer placing the order.
+     */
     @NotNull
     private final UUID customerId;
 
+    /**
+     * Unique identifier of the restaurant from which the order is placed.
+     */
     @NotNull
     private final UUID restaurantId;
 
+    /**
+     * Total price of the order.
+     */
     @NotNull
     private final BigDecimal price;
 
+    /**
+     * List of items included in the order.
+     */
     @NotNull
     private final List<OrderItem> items;
 
+    /**
+     * Delivery address for the order.
+     */
     @NotNull
     private final OrderAddress address;
 }
