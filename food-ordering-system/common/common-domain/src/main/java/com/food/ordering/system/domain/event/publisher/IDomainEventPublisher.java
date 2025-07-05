4bcf0c23464310707794b5b40c875bf2d2a5a692
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-05 14:44:03 -0300

    docs: Enhance documentation for IDomainEventPublisher interface

--- food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/event/publisher/IDomainEventPublisher.java
+++ food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/event/publisher/IDomainEventPublisher.java
@@ -2,7 +2,18 @@ package com.food.ordering.system.domain.event.publisher;
 
 import com.food.ordering.system.domain.event.IDomainEvent;
 
+/**
+ * Interface for publishing domain events.
+ *
+ * @param <T> the type of domain event to be published, must extend
+ *            {@link IDomainEvent}
+ */
 @SuppressWarnings("rawtypes")
 public interface IDomainEventPublisher<T extends IDomainEvent> {
+    /**
+     * Publishes the given domain event.
+     *
+     * @param domainEvent the domain event to publish
+     */
     void publish(T domainEvent);
 }
