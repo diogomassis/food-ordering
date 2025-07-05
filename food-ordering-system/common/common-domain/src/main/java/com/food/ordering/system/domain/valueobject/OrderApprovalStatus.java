d82348b2c7e946fc9733393ffd97f3d410d8e3df
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-05 13:30:12 -0300

    docs: Update documentation for OrderApprovalStatus enum

--- food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/OrderApprovalStatus.java
+++ food-ordering-system/common/common-domain/src/main/java/com/food/ordering/system/domain/valueobject/OrderApprovalStatus.java
@@ -1,5 +1,12 @@
 package com.food.ordering.system.domain.valueobject;
 
+/**
+ * Represents the approval status of an order in the food ordering system.
+ * <ul>
+ * <li>{@code APPROVED} - The order has been approved by the restaurant.</li>
+ * <li>{@code REJECTED} - The order has been rejected by the restaurant.</li>
+ * </ul>
+ */
 public enum OrderApprovalStatus {
     APPROVED, REJECTED
 }
