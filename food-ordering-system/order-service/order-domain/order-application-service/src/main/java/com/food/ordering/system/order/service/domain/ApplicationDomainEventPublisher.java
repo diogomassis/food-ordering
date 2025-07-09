c141283282313a1094df67b7a65948311e199b0c
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-08 21:09:20 -0300

    docs: Enhance documentation for ApplicationDomainEventPublisher class and its methods

--- food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ApplicationDomainEventPublisher.java
+++ food-ordering-system/order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ApplicationDomainEventPublisher.java
@@ -9,17 +9,40 @@ import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Publishes domain events using Spring's {@link ApplicationEventPublisher}.
+ * <p>
+ * This class implements {@link ApplicationEventPublisherAware} to receive the
+ * event publisher and {@link IDomainEventPublisher} to publish
+ * {@link OrderCreatedEvent} events.
+ * </p>
+ */
 @Slf4j
 @Component
 public class ApplicationDomainEventPublisher
         implements ApplicationEventPublisherAware, IDomainEventPublisher<OrderCreatedEvent> {
+
+    /**
+     * The Spring application event publisher used to publish domain events.
+     */
     private ApplicationEventPublisher applicationEventPublisher;
 
+    /**
+     * Sets the {@link ApplicationEventPublisher} instance.
+     *
+     * @param applicationEventPublisher the event publisher to use
+     */
     @Override
     public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
         this.applicationEventPublisher = applicationEventPublisher;
     }
 
+    /**
+     * Publishes the given {@link OrderCreatedEvent} using the application event
+     * publisher.
+     *
+     * @param domainEvent the event to publish
+     */
     @Override
     public void publish(OrderCreatedEvent domainEvent) {
         this.applicationEventPublisher.publishEvent(domainEvent);
