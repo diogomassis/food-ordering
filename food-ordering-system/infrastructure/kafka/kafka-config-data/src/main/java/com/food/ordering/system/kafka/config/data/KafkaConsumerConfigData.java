ac9e6b72233245d973df7a8abb678f6c2ab0609e
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-09 22:02:44 -0300

    feat: Add additional properties to KafkaConsumerConfigData class for enhanced configuration

--- food-ordering-system/infrastructure/kafka/kafka-config-data/src/main/java/com/food/ordering/system/kafka/config/data/KafkaConsumerConfigData.java
+++ food-ordering-system/infrastructure/kafka/kafka-config-data/src/main/java/com/food/ordering/system/kafka/config/data/KafkaConsumerConfigData.java
@@ -9,5 +9,19 @@ import lombok.Data;
 @Configuration
 @ConfigurationProperties(prefix = "kafka-consumer-config")
 public class KafkaConsumerConfigData {
-
+    private String keyDeserializer;
+    private String valueDeserializer;
+    private String autoOffsetReset;
+    private String specificAvroReaderKey;
+    private String specificAvroReader;
+    private Boolean batchListener;
+    private Boolean autoStartup;
+    private Integer concurrencyLevel;
+    private Integer sessionTimeoutMs;
+    private Integer heartbeatIntervalMs;
+    private Integer maxPollIntervalMs;
+    private Long pollTimeoutMs;
+    private Integer maxPollRecords;
+    private Integer maxPartitionFetchBytesDefault;
+    private Integer maxPartitionFetchBytesBoostFactor;
 }
