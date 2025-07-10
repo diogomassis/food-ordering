f7d9850e60b419a2c982cfc54a32722867691471
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-09 22:02:49 -0300

    feat: Add properties for Kafka producer configuration in KafkaProducerConfigData class

--- food-ordering-system/infrastructure/kafka/kafka-config-data/src/main/java/com/food/ordering/system/kafka/config/data/KafkaProducerConfigData.java
+++ food-ordering-system/infrastructure/kafka/kafka-config-data/src/main/java/com/food/ordering/system/kafka/config/data/KafkaProducerConfigData.java
@@ -9,5 +9,13 @@ import lombok.Data;
 @Configuration
 @ConfigurationProperties(prefix = "kafka-producer-config")
 public class KafkaProducerConfigData {
-
+    private String keySerializerClass;
+    private String valueSerializerClass;
+    private String compressionType;
+    private String acks;
+    private Integer batchSize;
+    private Integer batchSizeBoostFactor;
+    private Integer lingerMs;
+    private Integer requestTimeoutMs;
+    private Integer retryCount;
 }
