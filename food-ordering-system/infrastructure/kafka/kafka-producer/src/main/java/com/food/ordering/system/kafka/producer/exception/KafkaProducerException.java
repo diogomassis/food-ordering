298f52185e5739a9de423b5a55fac2681456d7ae
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-10 22:29:49 -0300

    docs: Improve documentation for KafkaProducerException class

--- food-ordering-system/infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/exception/KafkaProducerException.java
+++ food-ordering-system/infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/exception/KafkaProducerException.java
@@ -1,6 +1,14 @@
 package com.food.ordering.system.kafka.producer.exception;
 
+/**
+ * Exception thrown when an error occurs in the Kafka producer.
+ */
 public class KafkaProducerException extends RuntimeException {
+    /**
+     * Constructs a new KafkaProducerException with the specified detail message.
+     *
+     * @param message the detail message
+     */
     public KafkaProducerException(String message) {
         super(message);
     }
