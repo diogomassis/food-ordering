d19483ed887d79cf853d9a361b640228f786991c
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-10 22:27:39 -0300

    feat: Implement KafkaProducer class for message sending

---
+++ food-ordering-system/infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/service/impl/KafkaProducer.java
@@ -0,0 +1,48 @@
+package com.food.ordering.system.kafka.producer.service.impl;
+
+import java.io.Serializable;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
+
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.common.KafkaException;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import com.food.ordering.system.kafka.producer.exception.KafkaProducerException;
+import com.food.ordering.system.kafka.producer.service.IKafkaProducer;
+
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class KafkaProducer<K extends Serializable, V extends SpecificRecordBase> implements IKafkaProducer<K, V> {
+    private final KafkaTemplate<K, V> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<K, V> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @Override
+    public void send(String topicName, K key, V message, BiConsumer<SendResult<K, V>, Throwable> callback) {
+        try {
+            log.info("Sending message={} to topic={}", message, topicName);
+            CompletableFuture<SendResult<K, V>> kafkaResultFuture = kafkaTemplate.send(topicName, key, message);
+            kafkaResultFuture.whenComplete(callback);
+        } catch (KafkaException e) {
+            log.error("Error on kafka producer with key: {}, message: {} and exception: {}", key, message,
+                    e.getMessage());
+            throw new KafkaProducerException("Error on kafka producer with key: " + key + " and message: " + message);
+        }
+    }
+
+    @PreDestroy
+    public void close() {
+        if (kafkaTemplate != null) {
+            log.info("Closing kafka producer!");
+            kafkaTemplate.destroy();
+        }
+    }
+}
