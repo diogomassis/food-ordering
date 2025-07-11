ef02074f4debb8f36152f284b50f7a263d5aedda
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-10 22:29:54 -0300

    docs: Enhance IKafkaProducer interface documentation for clarity

--- food-ordering-system/infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/service/IKafkaProducer.java
+++ food-ordering-system/infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/kafka/producer/service/IKafkaProducer.java
@@ -6,6 +6,21 @@ import java.util.function.BiConsumer;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.springframework.kafka.support.SendResult;
 
+/**
+ * Interface for a Kafka producer service.
+ *
+ * @param <K> the type of the key, must be Serializable
+ * @param <V> the type of the value, must extend SpecificRecordBase
+ */
 public interface IKafkaProducer<K extends Serializable, V extends SpecificRecordBase> {
+    /**
+     * Sends a message to the specified Kafka topic.
+     *
+     * @param topicName the name of the Kafka topic
+     * @param key       the key of the message
+     * @param message   the message to send
+     * @param callback  a callback to handle the result or error of the send
+     *                  operation
+     */
     void send(String topicName, K key, V message, BiConsumer<SendResult<K, V>, Throwable> callback);
 }
