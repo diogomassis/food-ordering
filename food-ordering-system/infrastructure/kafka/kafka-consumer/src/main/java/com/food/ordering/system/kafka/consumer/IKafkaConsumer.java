5aa831244890b7ffa922ad3e353277c5df78b916
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-10 23:12:07 -0300

    docs: Add detailed Javadoc comments to IKafkaConsumer interface methods

--- food-ordering-system/infrastructure/kafka/kafka-consumer/src/main/java/com/food/ordering/system/kafka/consumer/IKafkaConsumer.java
+++ food-ordering-system/infrastructure/kafka/kafka-consumer/src/main/java/com/food/ordering/system/kafka/consumer/IKafkaConsumer.java
@@ -4,6 +4,20 @@ import java.util.List;
 
 import org.apache.avro.specific.SpecificRecordBase;
 
+/**
+ * Interface for a Kafka consumer that processes messages of a specific Avro
+ * type.
+ *
+ * @param <T> the type of the message, must extend SpecificRecordBase
+ */
 public interface IKafkaConsumer<T extends SpecificRecordBase> {
+    /**
+     * Receives and processes a batch of messages from Kafka.
+     *
+     * @param messages   the list of messages received
+     * @param keys       the list of message keys
+     * @param partitions the list of partition numbers
+     * @param offsets    the list of message offsets
+     */
     void receive(List<T> messages, List<Long> keys, List<Integer> partitions, List<Long> offsets);
 }
