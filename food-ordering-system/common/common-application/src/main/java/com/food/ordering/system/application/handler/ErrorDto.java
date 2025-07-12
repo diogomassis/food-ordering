9c3f499b005175cd65cda637a8fe464df9c1fa91
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-11 22:32:07 -0300

    docs: Enhance ErrorDto class with detailed documentation for error response fields

--- food-ordering-system/common/common-application/src/main/java/com/food/ordering/system/application/handler/ErrorDto.java
+++ food-ordering-system/common/common-application/src/main/java/com/food/ordering/system/application/handler/ErrorDto.java
@@ -4,10 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+/**
+ * Data Transfer Object representing an error response.
+ * Contains an error code and a descriptive message.
+ */
 @Data
 @Builder
 @AllArgsConstructor
 public class ErrorDto {
+    /**
+     * The unique code identifying the error.
+     */
     private final String code;
+
+    /**
+     * The human-readable message describing the error.
+     */
     private final String message;
 }
