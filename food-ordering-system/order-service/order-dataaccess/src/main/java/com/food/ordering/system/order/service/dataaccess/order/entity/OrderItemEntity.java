513fa0d349d77de5e25fbc0d139a6595efc29ae6
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-14 18:54:01 -0300

    feat: Add OrderItemEntity class for order item representation in the data access layer

---
+++ food-ordering-system/order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/entity/OrderItemEntity.java
@@ -0,0 +1,56 @@
+package com.food.ordering.system.order.service.dataaccess.order.entity;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@IdClass(OrderItemEntityId.class)
+@Table(name = "order_items")
+@Entity
+public class OrderItemEntity {
+    @Id
+    private UUID id;
+
+    @Id
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ORDER_ID")
+    private OrderEntity order;
+
+    private UUID productId;
+    private BigDecimal price;
+    private Integer quantity;
+    private BigDecimal subTotal;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, order);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        OrderItemEntity that = (OrderItemEntity) obj;
+        return id.equals(that.id) && order.equals(that.order);
+    }
+}
