11829810735c380a499f0283a1b01b9fab874684
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-14 18:54:45 -0300

    feat: Implement OrderAddressEntity class for order address representation in the data access layer

---
+++ food-ordering-system/order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/entity/OrderAddressEntity.java
@@ -0,0 +1,51 @@
+package com.food.ordering.system.order.service.dataaccess.order.entity;
+
+import java.util.Objects;
+import java.util.UUID;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "order_address")
+@Entity
+public class OrderAddressEntity {
+    @Id
+    private UUID id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ORDER_ID")
+    private OrderEntity order;
+
+    private String street;
+    private String postalCode;
+    private String city;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        OrderAddressEntity that = (OrderAddressEntity) obj;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
