c227ec081419541652935bfe905c693b587e5a71
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-14 18:53:28 -0300

    docs: Update documentation for OrderItemEntityId class to clarify composite key representation

--- food-ordering-system/order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/entity/OrderItemEntityId.java
+++ food-ordering-system/order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/entity/OrderItemEntityId.java
@@ -9,13 +9,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Composite primary key entity for OrderItem.
+ * This class represents the composite key used to uniquely identify
+ * an order item in the database, consisting of an ID and the associated order.
+ */
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderItemEntityId implements Serializable {
+    /**
+     * The unique identifier for the order item.
+     */
     private Long id;
+
+    /**
+     * The order entity that this order item belongs to.
+     * This forms part of the composite key along with the id.
+     */
     private OrderEntity order;
 
     @Override
