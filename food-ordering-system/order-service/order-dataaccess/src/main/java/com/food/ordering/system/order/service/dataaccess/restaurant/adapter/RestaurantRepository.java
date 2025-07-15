f73f63dc9202579f391784889b9194fb185df626
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-15 18:46:38 -0300

    feat: Add RestaurantRepository and RestaurantDataAccessMapper for restaurant data handling

---
+++ food-ordering-system/order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/restaurant/adapter/RestaurantRepository.java
@@ -0,0 +1,32 @@
+package com.food.ordering.system.order.service.dataaccess.restaurant.adapter;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.food.ordering.system.order.service.dataaccess.restaurant.entity.RestaurantEntity;
+import com.food.ordering.system.order.service.dataaccess.restaurant.mapper.RestaurantDataAccessMapper;
+import com.food.ordering.system.order.service.dataaccess.restaurant.repository.IRestaurantJpaRepository;
+import com.food.ordering.system.order.service.domain.entity.Restaurant;
+import com.food.ordering.system.order.service.domain.ports.output.repository.IRestaurantRepository;
+
+public class RestaurantRepository implements IRestaurantRepository {
+
+    private final IRestaurantJpaRepository restaurantJpaRepository;
+    private final RestaurantDataAccessMapper restaurantDataAccessMapper;
+
+    public RestaurantRepository(IRestaurantJpaRepository restaurantJpaRepository,
+            RestaurantDataAccessMapper restaurantDataAccessMapper) {
+        this.restaurantJpaRepository = restaurantJpaRepository;
+        this.restaurantDataAccessMapper = restaurantDataAccessMapper;
+    }
+
+    @Override
+    public Optional<Restaurant> findRestaurantInformation(Restaurant restaurant) {
+        List<UUID> restaurantProducts = restaurantDataAccessMapper.restaurantToRestaurantProducts(restaurant);
+        Optional<List<RestaurantEntity>> restaurantEntities = restaurantJpaRepository
+                .findByRestaurantIdAndProductIdIn(restaurant.getId().getValue(),
+                        restaurantProducts);
+        return restaurantEntities.map(restaurantDataAccessMapper::restaurantEntityToRestaurant);
+    }
+}
