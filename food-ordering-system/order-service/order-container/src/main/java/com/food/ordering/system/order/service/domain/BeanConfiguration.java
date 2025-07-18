488e489c87402fcb85338a739fa86657d2670797
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-17 21:03:03 -0300

    docs: Update documentation comments in BeanConfiguration class for clarity

--- food-ordering-system/order-service/order-container/src/main/java/com/food/ordering/system/order/service/domain/BeanConfiguration.java
+++ food-ordering-system/order-service/order-container/src/main/java/com/food/ordering/system/order/service/domain/BeanConfiguration.java
@@ -3,8 +3,17 @@ package com.food.ordering.system.order.service.domain;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration class for defining beans in the order service domain.
+ */
 @Configuration
 public class BeanConfiguration {
+
+    /**
+     * Creates and returns an instance of {@link IOrderDomainService}.
+     *
+     * @return a new instance of {@link OrderDomainService}
+     */
     @Bean
     public IOrderDomainService orderDomainService() {
         return new OrderDomainService();
