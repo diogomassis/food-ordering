88ee80eb537052434f19e1aba90a841bb465ebde
Diogo Martins de Assis <me@diogomartinsdeassis.com.br>
2025-07-28 20:42:37 -0300

    feat: Add repository interfaces for CreditEntry, CreditHistory, and Payment entities

---
+++ food-ordering-system/payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/output/repository/ICreditEntryRepository.java
@@ -0,0 +1,12 @@
+package com.food.ordering.system.payment.service.domain.ports.output.repository;
+
+import java.util.Optional;
+
+import com.food.ordering.system.domain.valueobject.CustomerId;
+import com.food.ordering.system.payment.service.domain.entity.CreditEntry;
+
+public interface ICreditEntryRepository {
+    CreditEntry save(CreditEntry creditEntry);
+
+    Optional<CreditEntry> findByCustomerId(CustomerId customerId);
+}
